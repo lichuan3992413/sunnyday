6b8169ee08f9e1fc51774489195ec1d403810189
lichuan <lichuan3992413@126.com>
2017-12-31 11:13:13 +0800

    'delete'

--- eclipse-workspace/gateway/src/main/java/sunnyday/gateway/util/ISmsService.java
+++
@@ -1,5 +0,0 @@
-package sunnyday.gateway.util;
-
-public interface  ISmsService {
-	public HSResponse doSomething(CommonParameter param);
-}
--- eclipse-workspace/gateway/src/main/java/sunnyday/gateway/util/Task.java
+++
@@ -1,5 +0,0 @@
-package sunnyday.gateway.util;
-
-public interface Task {
-	public boolean isFinshed();
-}
